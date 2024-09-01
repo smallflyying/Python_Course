308a3dbf7f6daafa6fc9bcf4b8123aab6f4cfe5a
lihongfei <2529574569@qq.com>
2024-09-01 23:13:23 +0800

    Python常用的转义字符

---
+++ 02章_Python语言概述/01_change_char.py
@@ -0,0 +1,23 @@
+# @Version  : 1.0
+# @Author   : LiHongFei
+# @File     : 01_change_char.py
+# @Time     : 2024/9/1 22:47
+# Python常用的转义字符
+
+# \t制表符
+print("jack\t20")
+# \n换行
+print("Hello,jack\nhello,tom")
+
+# \\ 输出 \
+print("D:\\python\\chapter02")
+
+# \" 输出 "
+print("杨过说：\"小龙女你嫁给我吧\"")
+
+# \' 输出 '
+print("郭靖说：\'hello\'")  # 单引号也可以不使用转义 \
+print("郭靖说：'hello'")  # 单引号也可以不使用转义 \
+
+# \r是回车
+print("段誉向西行，\r他虽不会武功")
---
+++ 02章_Python语言概述/02_change_char_exercise.py
@@ -0,0 +1,10 @@
+# @Version  : 1.0
+# @Author   : LiHongFei
+# @File     : 02_change_char_exercise.py
+# @Time     : 2024/9/1 22:57
+
+# 要求：请使用条输出语句，达到输出如下图形的效果
+# 姓名    年龄  籍贯  住址
+# john    12   河北  北京
+
+print("姓名\t\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京")
