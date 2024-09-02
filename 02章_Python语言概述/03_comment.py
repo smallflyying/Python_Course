293c921103a810aaa9c94f27cc9463719eb5bcdf
lihongfei <2529574569@qq.com>
2024-09-02 22:55:18 +0800

    注释和内置函数使用

---
+++ 02章_Python语言概述/03_comment.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+# @Version  : 1.0
+# @Author   : LiHongFei
+# @File     : 03_comment.py
+# @Time     : 2024/9/2 22:10
+
+print("hello jack")
+print("hello tom")
+print("hello mary")
+print("hello hsp")
+# 使用''' '''进行多行注释
+'''
+print("hello smith")
+print("hello john")
+print("hello milan")
+'''
+# 或者使用"""进行多行注释
+"""
+print("hello smith")
+print("hello john")
+print("hello milan")
+"""
---
+++ 02章_Python语言概述/04_abs_fuc.py
@@ -0,0 +1,10 @@
+# @Version  : 1.0
+# @Author   : LiHongFei
+# @File     : 04_abs_fuc.py
+# @Time     : 2024/9/2 22:52
+
+# 演示内置函数 abs的使用
+
+print("-100的绝对值", abs(-100))
+print("-100.67的绝对值", abs(-100.67))
+print("200的绝对值", abs(200))
