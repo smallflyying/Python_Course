447da4ffd42d9016c0d936c0a216cb5b40e89763
lihongfei <2529574569@qq.com>
2024-09-06 22:12:32 +0800

    数据类型转换

---
+++ 03章_变量/10_数据类型转换问题.py
@@ -0,0 +1,8 @@
+# @Version  : 1.0
+# @Author   : LiHongFei
+# @File     : 10_数据类型转换问题.py
+# @Time     : 2024/9/6 22:10
+
+n1 = 100
+result = "n1的值是: " + n1
+print(result)  # TypeError: can only concatenate str (not "int") to str
