859d669d254a555179395221949ef23e1f682f82
lihongfei <2529574569@qq.com>
2024-09-10 23:33:01 +0800

    进制

---
+++ 05章_进制/01_binary_.py
@@ -0,0 +1,16 @@
+# @Version  : 1.0
+# @Author   : LiHongFei
+# @File     : 01_binary_.py
+# @Time     : 2024/9/10 23:12
+
+# 2 进制
+print(0b111)
+print(0B111)
+# 8 进制
+print(0o111)
+print(0O111)
+# 10 进制
+print(111)
+# 16 进制
+print(0x111)
+print(0X111)
