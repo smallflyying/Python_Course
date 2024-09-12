439ca4fd034183afd8f6024725107506dafcfc34
lihongfei <2529574569@qq.com>
2024-09-12 23:18:56 +0800

    位运算符

--- 05章_进制/02_bit_operator.py
+++ 05章_进制/02_bit_operator.py
@@ -5,4 +5,21 @@
 
 # 位运算的案例
 
-print(~-2)
+print(~-2)  # 1
+print(~2)   # -3
+print(2 & 3)  # 2
+print(2 ^ -3)  # -1
+print(2 | 3)  # 3
+
+print(5 << 1)  # 10
+print(-5 << 1) # -10
+print(7 << 2)  # 28
+print(-7 << 2)  # -28
+print(5 >> 1)  # 2
+print(-5 >> 1)  # -3
+
+print(8 >> 2)  # 2
+print(-13 >> 2) # -4
+
+print(1 >> 2)  # 0
+print(-1 >> 2)  # -1
