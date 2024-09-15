af4816462e0fbc88a87f2d37ec5cd90a6e3a1906
lihongfei <2529574569@qq.com>
2024-09-16 00:05:38 +0800

    嵌套循环练习题

---
+++ 06_程序控制结构/25_multiply_for.py
@@ -0,0 +1,21 @@
+# @Version  : 1.0
+# @Author   : LiHongFei
+# @File     : 25_multiply_for.py
+# @Time     : 2024/9/15 11:15
+
+# 如果外层循环次数为m次，内层为n次，则内层循环体实际上需要执行m*n次,
+# count = 0
+for i in range(3):
+    for j in range(5):
+        print("i = ", i, " j = ", j)
+        # for k in range(2):
+        #     count += 1
+        #     print("ok")
+# print(count)
+
+
+# for i in [0, 1]:
+#     for j in [0, 1, 2]:
+#         print("i = ", i, " j = ", j)
+        # print("ok")
+
