3d4fcc1b0b31d2fbe0ca27e2480ab704f1a239a0
lihongfei <2529574569@qq.com>
2024-09-21 23:02:19 +0800

    列表注意事项和使用细节

---
+++ 08章_数据容器/07_list_operations.py
@@ -0,0 +1,18 @@
+# @Version  : 1.0
+# @Author   : LiHongFei
+# @File     : 07_list_operations.py
+# @Time     : 2024/9/20 23:28
+
+
+# 演示列表常用操作
+list_a = [100, 200, 300, 400, 600]
+print("list_a 列表元素个数: ", len(list_a))
+print("list_a 列表最大元素: ", max(list_a))
+print("list_a 列表最小元素: ", min(list_a))
+
+# list.append(obj) : 在列表末尾添加新的对象
+# 请在list_a 列表后，添加 900
+print("list_a: ")
+
+# list.count(obj) : 统计某个元素在列表中出现的次数
+print("100出现次数是: ")
