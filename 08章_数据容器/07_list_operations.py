5843bfcb96b9b8c83a8f480b49d4915b8dfa9267
lihongfei <2529574569@qq.com>
2024-09-21 23:44:56 +0800

    列表常用操作

--- 08章_数据容器/07_list_operations.py
+++ 08章_数据容器/07_list_operations.py
@@ -12,7 +12,31 @@ print("list_a 列表最小元素: ", min(list_a))
 
 # list.append(obj) : 在列表末尾添加新的对象
 # 请在list_a 列表后，添加 900
-print("list_a: ")
+list_a.append(900)
+# list_a.append(100)
+print("list_a: ", list_a)   # [100, 200, 300, 400, 600, 900]
 
 # list.count(obj) : 统计某个元素在列表中出现的次数
-print("100出现次数是: ")
+print("100出现次数是: ", list_a.count(100))   # 1
+
+# list.extend(seq)：在列表末尾一次性追加另一个序列中的多个值（用新列表扩展原来的列表）
+list_b = [1, 2, 3]
+# 将list_b追加到list_a
+list_a.extend(list_b)
+print("list_a: ", list_a)  # [100, 200, 300, 400, 600, 900, 1, 2, 3]
+
+
+# list.index(obj)：从列表中找到某个值的第一个匹配项的索引位置
+# 如果找不到,会报错: ValueError
+print("300第一次出现在序列的索引是: ", list_a.index(300))  # 2
+# print("3000最后一次出现在序列的索引是: ", list_a.index(3000))  # ValueError: 3000 is not in list
+
+# 翻转list_a, list_a.reverse()
+list_a.reverse()
+print("list_a: ", list_a)  # [3, 2, 1, 900, 600, 400, 300, 200, 100]
+
+
+# list.insert(index, obj): 将对象插入列表指定的index位置
+# 请实现把666插入到index 为1的位置
+list_a.insert(1, 666)
+print("list_a: ", list_a)  # [3, 666, 2, 1, 900, 600, 400, 300, 200, 100]
